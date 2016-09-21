8f6db9e57f0eed5c802adea767e82d81a1c54b82
Kevin Destrem <kevin@destrem.net>
2016-09-22 05:17:23 +0900

    Fix the return value of the build Script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
 echo "Run Unity Tests"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -runEditorTests -quit -projectPath $(pwd)
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -runEditorTests \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -quit
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+exit $?
